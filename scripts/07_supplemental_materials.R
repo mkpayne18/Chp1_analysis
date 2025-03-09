b15bfbd039c19d18d117bcf88a7705f4ee1cdc99
Molly Payne <mpayne19@alaska.edu>
2025-03-09 10:25:42 -0800

    change appendix order number, rm row names from output

--- scripts/07_supplemental_materials.R
+++ scripts/07_supplemental_materials.R
@@ -6,7 +6,7 @@
 
 # Purpose of this script: Create supplemental tables and figs for thesis/manuscript
 
-# Last updated: December 19, 2023
+# Last updated: March 9, 2025
 #-------------------------------------------------------------------------------
 library(ggplot2)
 library(ggpubr)
@@ -158,24 +158,24 @@ deads_by_yr <- survey_data8 %>% group_by(StreamName, Year) %>%
   summarise(Total_dead = sum(DeadCount),
             Number_H_fish = sum(NumberStrays),
             Total_fish_sampled = sum(NumberofSpecimens))
-tables5 <- left_join(new_response_var, deads_by_yr, by = c("StreamName", "Year"))
+s2tables1 <- left_join(new_response_var, deads_by_yr, by = c("StreamName", "Year"))
 
 sapply(deads_by_yr, function(x) sum(is.na(x)))
 
-tables5 <- tables5[,c(2,1,8,7,6,3:5)]
-colnames(tables5)[2] <- "Stream name"      
-colnames(tables5)[3] <- "Total fish sampled"
-colnames(tables5)[4] <- "Number of hatchery fish"
-colnames(tables5)[5] <- "Dead count"
-colnames(tables5)[6] <- "Effective number of hatchery strays"
-colnames(tables5)[7] <- "Number of surveys"
-colnames(tables5)[8] <- "Attractiveness index (average effective number of strays)"
+s2tables1 <- s2tables1[,c(2,1,8,7,6,3:5)]
+colnames(s2tables1)[2] <- "Stream name"      
+colnames(s2tables1)[3] <- "Total fish sampled"
+colnames(s2tables1)[4] <- "Number of hatchery fish"
+colnames(s2tables1)[5] <- "Dead count"
+colnames(s2tables1)[6] <- "Effective number of hatchery strays"
+colnames(s2tables1)[7] <- "Number of surveys"
+colnames(s2tables1)[8] <- "Attractiveness index (average effective number of strays)"
 
 #Reduce the number of digits
-tables5$`Effective number of hatchery strays` <-
-  round(tables5$`Effective number of hatchery strays`, 1)
-tables5$`Attractiveness index (average effective number of strays)` <-
-  round(tables5$`Attractiveness index (average effective number of strays)`, 1)
+s2tables1$`Effective number of hatchery strays` <-
+  round(s2tables1$`Effective number of hatchery strays`, 1)
+s2tables1$`Attractiveness index (average effective number of strays)` <-
+  round(s2tables1$`Attractiveness index (average effective number of strays)`, 1)
 
 #NOTE that the effective number of strays col does not always = number of
 #hatchery fish / proportion sampled (where proportion sampled = total fish
@@ -185,7 +185,7 @@ tables5$`Attractiveness index (average effective number of strays)` <-
 #slightly greater than would be expected. See docs/effective_number_strays_note.
 #Rmd for more info
 
-# write.csv(tables5, "figs/supplemental/stream_year_info.csv")
+write.csv(s2tables1, "figs/supplemental/appendixS2_tabs1.csv", row.names = F)
 
 
 
